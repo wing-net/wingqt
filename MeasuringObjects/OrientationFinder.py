2ac947241bbec7e625ff7151651faa796c2b5abb
wing-net <71534160+wing-net@users.noreply.github.com>
2020-11-16 01:17:42 -0700

    Add files via upload

---
+++ MeasuringObjects/OrientationFinder.py
@@ -0,0 +1,58 @@
+import cv2
+import math
+
+
+# Divides the wing's contour into two pieces: the middle and the ends.
+def divideWing(hull):
+    tip1 = [ ]
+    tip2 = [ ]
+
+    for i in range(len(hull)):
+        mid = int(len(hull) / 2)
+        lobound = int(mid / 2)
+        upbound = int((len(hull) + mid) / 2)
+        # This will extract points on one side of the wing
+        if (i >= lobound) & (i <= upbound):
+            # cv2.circle(orig, (hull[i][0][0],hull[i][0][1]), 8, (0, 255, 255), -1)
+            tip1.append((hull[ i ][ 0 ][ 0 ], hull[ i ][ 0 ][ 1 ]))
+        else:  # This will extract the points on the opposite end of the wing
+            # cv2.circle(orig, (hull[ i ][ 0 ][ 0 ], hull[ i ][ 0 ][ 1 ]), 8, (255, 0, 255), -1)
+            tip2.append((hull[ i ][ 0 ][ 0 ], hull[ i ][ 0 ][ 1 ]))
+    return tip1, tip2
+
+
+# Calculates the distance between points on an end of the wing
+def distanceToTip(tip):
+    distAvg = 0
+    for i in range(len(tip)):
+        if i + 1 < len(tip):
+            distAvg += dist(tip[ i ], tip[ i + 1 ])
+    return distAvg
+
+
+# Calculates the distance between two points
+def dist(p1, p2):
+    (x1, y1), (x2, y2) = p1, p2
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+def orientation(h_len, v_len, hull, orig):
+    if h_len > v_len:
+        tip1, tip2 = divideWing(hull)
+
+        # Finds the distance between corner points in the polygon
+        # A close average distance would mean the ploygon is curving
+        # Which is indicitive of a the tip of the wing
+        distAvg1 = distanceToTip(tip1) / (len(tip1))
+        distAvg2 = distanceToTip(tip2) / (len(tip2))
+
+        if distAvg1 < distAvg2:
+            return 'left'
+            # print('Wing tip is on the left side')
+        else:
+            return 'right'
+            # print('wing tip is on the right side')
+    else:
+        # This is where the implementation for top or bottom tip facing wings goes
+        return 'top'
+
--- MeasuringObjects/main.py
+++ MeasuringObjects/main.py
@@ -1,27 +1,37 @@
-import os
-from pointFinder import findContour, findSmallContours
 from process_image import conv_blkwht, resize, thresh_img, fill_img, split
+from PointFinder import findContour
 import cv2
+import argparse
+
 
 
 def main():
-    path = 'C:\\Users\\monic\\PycharmProjects\\MeasuringObjectsRevised\\images'
-    image_dir = os.listdir(path)
-    for img in image_dir:
-        original = cv2.imread(f'{path}/{img}')
-        print(img)
-        print('----------')
-       # print("Original: " + str(original.dtype))
-        edged = thresh_img(original)
-       #print("edged: " + str(edged.dtype))
-        flooded = fill_img(edged)
-        #print("Flooded " + str(flooded.dtype))
-        cv2.imwrite('temp2.JPG', conv_blkwht(flooded))
-        bin_img = cv2.imread('./temp2.JPG')
-        bin_img = thresh_img(bin_img)
-        #print("bin_img: " + str(bin_img.dtype))
-        findContour(original, bin_img, edged)
-        print('\n')
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True,
+                    help="path to the input image")
+    args = vars(ap.parse_args())
+
+    original = cv2.imread(args["image"])
+    # print('----------')
+    # print(args["image"])
+
+    edged = thresh_img(original)
+    flooded = fill_img(edged)
+    cv2.imwrite('temp2.JPG', conv_blkwht(flooded))
+    bin_img = cv2.imread('./temp2.JPG')
+    bin_img = thresh_img(bin_img)
+    start, end, original = findContour(original, bin_img, edged)
+    # print(start)
+    # print(type(start))
+    # print(end)
+    # print(type(end))
+    #findContour(original, bin_img, edged)
+    cv2.circle(original, start, 8, (0, 255, 0), -1)
+    cv2.circle(original, end, 8, (0, 255, 0), -1)
+    print(start, end)
+    resize(original)
+    cv2.waitKey(0)
 
 
 
